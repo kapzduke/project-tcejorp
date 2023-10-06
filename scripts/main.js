dd15ef78f5a8f19ab16c5d262c819dc0b36b0d14
kapzduke <60543005+kapzduke@users.noreply.github.com>
2023-10-06 16:25:25 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 Events.on(ContentInitEvent, e => {
     //gaptika = 256
-    Blocks.titaniumWall.envDisabled = Blocks.titaniumWallLarge.envDisabled = Blocks.door.envDisabled = Blocks.doorLarge.envDisabled = 272;
-    Blocks.container.envDisabled = Blocks.unloader.envDisabled = Blocks.shipFabricator.envDisabled = 256;
+    Blocks.titaniumWall.envDisabled = Blocks.titaniumWallLarge.envDisabled = Blocks.thoriumWall.envDisabled = Blocks.thoriumWallLarge.envDisabled = Blocks.door.envDisabled = Blocks.doorLarge.envDisabled = 272;
+    Blocks.container.envDisabled = Blocks.vault.envDisabled = Blocks.disperse.envDisabled = Blocks.unloader.envDisabled = Blocks.shipFabricator.envDisabled = 256;
     Items.scrap.buildable = false;
     //inomal = 512
     Blocks.surgeWall.envDisabled = Blocks.surgeWallLarge.envDisabled = 528
